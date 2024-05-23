be78ceb0c1ba3f217405dce28b237f0f91c3a878
Hilal-Bilun-Kara <bilunkara@gmail.com>
2024-05-23 20:04:35 +0300

    S13G2 Done

---
+++ untitled/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        public class Question {
+            public static void min(double firstNum, double secondNum){
+                System.out.println(firstNum - secondNum);
+            }
+            public static void min(int firstNum, int secondNum){
+                System.out.println(firstNum - secondNum);
+            }
+        }
+
+
+        public class Question {
+            public static String concat(String firstName, String lastName){
+                return firstName + " " + lastName;
+            }
+            public static String concat(String firstName, String lastName,int age){
+                return firstName + " " + lastName + " " + age;
+            }
+        }
+    }
+}
